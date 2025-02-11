04f0b43f221307db7e3fe256fe92242d96cc4512
diogenesbrussels <olivier@diogenes.brussels>
2025-02-11 14:22:50 +0100

    add progress counter

--- remove_apps.sh
+++ remove_apps.sh
@@ -12,17 +12,23 @@ adb devices
 echo "If the device shows up press any key to continue... "
 read -s -n 1
 
+# Count total lines (total apps to remove)
+total=$(wc -l < toremove.txt)
+current=0
+
 while read p; do
-	echo "Trying to uninstall $p ..."
-	# Two methods to remove the app
-	adb exec-out "pm uninstall --user 0 $p"
-	adb exec-out "pm uninstall $p"
-	echo "----------------------------"
-done <toremove.txt
+    ((current++))
+    echo "[$current/$total] Trying to uninstall $p ..."
+    
+    adb exec-out "pm uninstall --user 0 $p"
+    adb exec-out "pm uninstall $p"
+    
+    echo "----------------------------"
+done < toremove.txt
 
-echo "### Uninstall completed ! ###"
+echo "### Uninstall completed! ###"
 
-echo "Press any key to disable usb debug & dev mode... "
+echo "Press any key to disable USB debug & dev mode... "
 read -s -n 1
 
 adb exec-out "settings put global adb_enabled 0"
@@ -30,3 +36,4 @@ adb exec-out "settings put global adb_enabled 0"
 echo ""
 echo "### Bye bye ###"
 echo ""
+
